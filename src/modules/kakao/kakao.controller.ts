cc5d6b0d7dde00443caa2696a763e3b50f9aabf7
Defor0127 <721snglwhs@naver.com>
2026-04-02 11:25:06 +0900

    feat: 카카오 챗봇 테스트 컨트롤러 추가

--- src/modules/kakao/kakao.controller.ts
+++ src/modules/kakao/kakao.controller.ts
@@ -16,6 +16,10 @@ export class KakaoController {
     // 2. 서버 로그에 출력 (데이터 확인용)
     console.log('--- 데이터 추출 결과 ---');
     console.log('body', body);
+    console.log('body.userRequest', body.userRequest);
+    console.log('body.userRequest.utterance', body.userRequest.utterance);
+    console.log('body.userRequest.user', body.userRequest.user);
+    console.log('body.userRequest.user.id', body.userRequest.user.id);
     console.log(`사용자 ID: ${userId}`);
     console.log(`메시지: ${utterance}`);
     console.log(`시간: ${serverTime}`);
